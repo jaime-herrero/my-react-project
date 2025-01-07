42d16f24dccf55225a086d162015d266b8601bc6
jaime-herrero <misdrev_305@msn.com>
2025-01-07 10:57:25 +0100

    MovieList

--- my-react-project/src/App.jsx
+++ my-react-project/src/App.jsx
@@ -5,6 +5,7 @@ import './App.css'
 import HeaderComponent from './Components/HeaderComponent'
 import ButtonComponent from './Components/ButtonComponent'
 import Login from './Components/login'
+import MovieList from './Components/MovieList'
 
 // Version prueba subida git 4
 
@@ -27,6 +28,8 @@ function App() {
   //   username: 'Jaime',
   //   email: 'jaime@email.com'
   // })
+
+  const condition = true;
 
   const [user, setUser] =  useState({})
 
@@ -54,11 +57,22 @@ function App() {
       <HeaderComponent greetingsValue={greetings} linksValue={links}></HeaderComponent>
 
       <main className='main-content'>
-       <h2 onClick={sayHello}>Jelou {user.username}!</h2>
+       {user.username && <h2 onClick={sayHello}>Jelou {user.username}!</h2>}
 
        <Login handleLogin={login}></Login>
-       
+
+       {condition && <h2>La condición se cumple</h2>}
+       {!condition && <h2>La condición NO se cumple</h2>} 
+
+       {condition ? (
+        <h2>La condición se cumple ternario</h2>
+       ) : (
+       <h2>La condición NO se cumple ternario</h2>
+       )}
+ 
        <h2 onClick={addOne}>Number: {number}</h2>
+
+       <MovieList></MovieList>
        
        <br/>
        <input value={myValue} placeholder={myPlaceholder} type='text' onChange={handleChange}/>
---
+++ my-react-project/src/Components/MovieList.jsx
@@ -0,0 +1,16 @@
+function MovieList() {
+    const movies = ["Project X","La vida de Brian","American Pie"];
+
+
+  return (
+    <section>
+    <h2>Movies</h2>
+
+    {movies}
+    </section>
+
+  )
+}
+
+
+export default MovieList
